8f69cf3ec87787351e94b6142f87eb92fe318e82
katscap <katielaw97@gmail.com>
2021-07-21 12:06:22 -0400

    testing config api file

---
+++ config.example.js
@@ -0,0 +1,8 @@
+module.exports = {
+  API_KEY: "API TOKEN HERE"
+}
+
+
+
+// make a file called config.js and copy this code over.
+// this will ensure that your api tokens don't get pushed to github.
