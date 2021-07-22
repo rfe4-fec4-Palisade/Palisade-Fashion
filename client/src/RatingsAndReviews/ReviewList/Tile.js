53a9946641f778ba739d0abb720568056383fe5f
katscap <katielaw97@gmail.com>
2021-07-22 19:41:52 -0400

    added basic css

--- client/src/RatingsAndReviews/ReviewList/Tile.js
+++ client/src/RatingsAndReviews/ReviewList/Tile.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import dateParser from '../../helperFunctions/dateParser.js'
+import dateParser from '../../helperFunctions/dateParser.js';
+import styled from 'styled-components';
+
+const Tile = styled.div`
+    border-bottom: 1px solid grey;
+    font-family: Helvetica, sans-serif;
+    font-size: 12px;
+    color: grey;
+`;
+
+const Summary = styled.div`
+    font-weight: bold;
+`;
 
 const ReviewTile = (props) => {
   console.log('hello from review tile', props.review.date)
   console.log('checking to see if date Parser works', dateParser(props.review.date))
   return (
-    <div>
+    <Tile>
       <div> STAR RATING HERE </div>
       <div>{props.review.reviewer_name}</div>
       <div>{dateParser(props.review.date)}</div>
-      <div>{props.review.summary}</div>
+      <Summary>{props.review.summary}</Summary>
       <div>{props.review.body}</div>
       {props.review.recommend ?
         <div> ✓ I recommend this product </div> : null}
       {props.review.response ?
         <div>
-          Response from seller:
+          Response from seller
           {props.review.response}
           </div>
           : null}
@@ -26,7 +38,7 @@ const ReviewTile = (props) => {
         <span>
       </div> */}
 
-    </div>
+    </Tile>
   )
 }
 
