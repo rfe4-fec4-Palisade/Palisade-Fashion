870daa25ef72486a35d49f63f3b52b68e80c1238
Liam Houston <liamhouston@Liams-MacBook-Air.local>
2021-07-26 13:53:31 -0400

    Made rough cards, not dynamic yet

--- client/src/RelatedItems/RelatedItemsList/Card.js
+++ client/src/RelatedItems/RelatedItemsList/Card.js
@@ -1,10 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Card = () => {
+const Card = (props) => {
+
+  const [card, updateCard] = useState({})
+
+  var id = props.id;
+  const getProduct = (id) => {
+    axios.get(`http://localhost:3000/products/${id}`)
+    .then(result => updateCard(result.data));
+  }
+
+  useEffect(() => {
+    getProduct(id)
+  }, [])
+
+  console.log(card);
   return (
     <div>
-      <div>A card</div>
+      <h2>{id}: {card.name}</h2>
+      <div>{card.slogan}</div>
+      <div>{card.description}</div>
+      <div>{card.catefory}</div>
     </div>
   )
 }
