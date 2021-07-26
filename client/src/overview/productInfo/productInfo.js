297acc5089f099fb9a955c7638b7bd7574ccebdb
SandraM1 <sministro@gmail.com>
2021-07-26 12:59:48 -0400

    refactored overview components with React Hooks

--- client/src/overview/productInfo/productInfo.js
+++ client/src/overview/productInfo/productInfo.js
@@ -1,1 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Price from './price.js';
+
+function ProductInfo () {
+  return (
+    <div>
+
+      {/* <StarsComponent/> */}
+
+      {/* // onclick send user to Reviews section */}
+      <a href="#">Read All Reviews</a>
+      <br></br>
+      <span>Category: 'get category from category property'</span>
+      <br></br>
+      <span>Product Name: 'get product name from name property'</span>
+      <br></br>
+      <Price/>
+
+    </div>
+  )
+}
+
+export default ProductInfo;
