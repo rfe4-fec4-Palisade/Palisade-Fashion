4961de8e478e6d8dc23612c4649f71d65744b24f
katscap <katielaw97@gmail.com>
2021-07-28 14:55:44 -0400

    implemented ratings percentage bars but need to work on css

--- client/src/RatingsAndReviews/RatingBreakdown/Breakdown.js
+++ client/src/RatingsAndReviews/RatingBreakdown/Breakdown.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import StarRating from './StarRating';
 
 const Breakdown = (props) => {
-
+  if( props.metadata.ratings === undefined ) {
+    return null;
+  }
   const [total, setTotal] = useState(0);
+  const [bars, setBars] = useState([1,2,3,4,5])
   /*
   {
     "product_id": "19089",
@@ -16,15 +20,23 @@ const Breakdown = (props) => {
 }
 
   approach for rating breakdown: go into metadata.ratings and total up all the values. set state total.
-  */
 
-  useEffect(()=>{
-    // let ratingTotal = props.metadata.ratings.1 + props.metadata.ratings.2 + props.metadata.ratings.3 + props.metadata.ratings.4;
-    console.log('this is ratingTotal', props.metadata);
+  create a state for each rating [1,2,3,4,5]. map over this array and pass in each number, as well as the value
+  */
+ useEffect(()=>{
+    const ratings = props.metadata.ratings
+    setTotal(+ratings["1"] + +ratings["2"]+ +ratings["3"] + +ratings["4"] + +ratings["5"])
   }, [])
 
+
+    // let ratingTotal = props.metadata.ratings.1 + props.metadata.ratings.2 + props.metadata.ratings.3 + props.metadata.ratings.4;
+
+
+
     return (
-      <div> This is the rating breakdown component</div>
+      <div>
+        {bars.map((bar) => <StarRating key={bar} total={total} count={props.metadata.ratings[bar]} rating={bar}/>)}
+      </div>
     )
 }
 
